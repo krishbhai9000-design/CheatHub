96a310a1e56d8586571e1187484cd53f52ff070b
krishna kumar <krishna kumar@DESKTOP-3JI2G22>
2026-03-19 22:58:31 +0530

    Add project files.

---
+++ CheatHub/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheatHub.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Free()
+        {
+            return View();
+        }
+
+        public IActionResult Paid()
+        {
+            return View();
+        }
+
+        public IActionResult Premium()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheatHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CheatHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
